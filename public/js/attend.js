6473f4b493eb633033e66bd1d3aa95d97247a202
Mirko Westermeier <mirko@westermeier.de>
2019-09-29 14:50:06 +0200

    Add minimal websocket connection demo

--- public/js/attend.js
+++ public/js/attend.js
@@ -2,12 +2,34 @@ new Vue({
     el: '#attend',
     data() { return {
         session: null,
-        userId: null
+        userId: null,
+        wsConnected: false,
+        wsURL: null,
     }},
+    methods: {
+        connect() {
+            if (! this.wsURL) return;
+            this.socket = new WebSocket(this.wsURL);
+            this.socket.onopen = () => {
+                this.wsConnected = true;
+                console.log('Connected...');
+                this.socket.onmessage = e => console.log(e);
+            };
+        },
+        disconnect() {
+            this.socket.close();
+            this.wsConnected = false;
+            console.log('Disconnected');
+        },
+        sync() {
+            this.socket.send('foo');
+        }
+    },
     mounted() {
         axios.get('/data.json').then(res => {
             this.session = res.data.session;
             this.userId  = res.data.userId;
+            this.wsURL   = res.data.wsURL;
         });
     }
 })
