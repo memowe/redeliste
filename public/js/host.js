c6a6ecda8168a841bff8565976aee935c90abc79
Mirko Westermeier <mirko.westermeier@uni-muenster.de>
2019-11-15 20:09:57 +0100

    Add simple chair frontend

---
+++ public/js/host.js
@@ -0,0 +1,43 @@
+new Vue({
+    el: '#host',
+    data() { return {
+        session: null,
+        nextSpeakers: null,
+        userId: null,
+        wsConnected: false,
+        wsURL: null,
+    }},
+    methods: {
+        connect() {
+            if (! this.wsURL) return;
+            this.socket = new WebSocket(this.wsURL);
+            this.socket.onopen = () => {
+                this.wsConnected = true;
+            };
+            this.socket.onmessage = msg => {
+                let data = JSON.parse(msg.data);
+                this.session      = data.session;
+                this.nextSpeakers = data.nextSpeakers;
+            };
+        },
+        disconnect() {
+            this.socket.close();
+            this.wsConnected = false;
+            this.session     = null;
+        },
+        requestSpeak() {
+            this.socket.send('RQSP');
+        },
+        callSpeaker() {
+            this.socket.send('NEXT');
+        },
+    },
+    mounted() {
+        axios.get('/data.json').then(res => {
+            this.session = res.data.session;
+            this.userId  = res.data.userId;
+            this.wsURL   = res.data.wsURL;
+            this.connect();
+        });
+    }
+})
