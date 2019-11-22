a713082af184ff7094c1a1b54d67eaab514e5241
Mirko Westermeier <mirko@westermeier.de>
2019-11-22 11:57:44 +0100

    Move session start and join routes to app

--- lib/Redeliste.pm
+++ lib/Redeliste.pm
@@ -51,6 +51,45 @@ sub startup ($self) {
 	# Static pages
 	$r->get('/')->name('choose');
 	$r->get("/$_") for qw(start join bye);
+
+    # Create a session and generate a token for session members
+    $r->post('/start' => sub ($c) {
+
+        # Init
+        my $session = $c->model->add_session(name => $c->param('name'));
+        my $chair   = $session->add_person(
+            name => $c->param('pname') // ('Anonymous' . int(rand 10_000)),
+            star => $c->param('sex') ne 'male'
+        );
+
+        # Store and work with it
+        $c->session(
+            token       => $session->token,
+            person_id   => $chair->id,
+            role        => 'chair',
+        )->redirect_to('host');
+    });
+
+    # Join a session
+    $r->post('/join' => sub ($c) {
+
+        # Lookup session
+        my $session = $c->model->sessions->{$c->param('token')};
+        return $c->reply->not_found unless defined $session;
+
+        # Inject data
+        my $person = $session->add_person(
+            name => $c->param('name') // ('Anonymous' . int(rand 10_000)),
+            star => $c->param('sex') ne 'male',
+        );
+
+        # Store and work with it
+        $c->session(
+            token       => $session->token,
+            person_id   => $person->id,
+            role        => 'user'
+        )->redirect_to('attend');
+    });
 }
 
 1;
